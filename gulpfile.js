1e48584a2761931c6f793cca16537d111023f488
shanchunyu <1774331872@qq.com>
2018-09-02 21:13:34 +0800

    css任务

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+
+var sass = require("gulp-sass");
+
+var minCss = require("gulp-clean-css");
+
+//编译sass压缩css
+gulp.task("devCss", function() {
+        return gulp.src("./src/scss/*.scss")
+            .pipe(sass())
+            .pipe(minCss())
+            .pipe(gulp.dest("./src/css"))
+    })
+    //监听
+
+gulp.task("watch", function() {
+    return gulp.watch("./src/scss/*.scss", gulp.series("devCss"));
+})
+
+//整合任务
+gulp.task("dev", gulp.series("devCss", "watch"));
